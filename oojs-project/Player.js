11bdc6d5f1a86054cb68dab31cff8a79f6631d6d
James Duffy <jduffy2000@gmail.com>
2018-11-29 09:02:35 -0500

    Getting started

---
+++ oojs-project/Player.js
@@ -0,0 +1,10 @@
+
+class Player {
+  constructor(name, id, color, active = false){
+    this.name = name;
+    this.id = id;
+    this.color = color;
+    this.active = active;
+    this.tokens = [];
+  }
+}
